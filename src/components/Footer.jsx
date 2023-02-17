267b5b160a4d6d7d69d3647ad699a70706fa06f7
canutoilberto <canutoilberto@gmail.com>
2023-02-17 13:40:00 -0300

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,70 @@
+import React from "react";
+
+import {
+  FaDribbbleSquare,
+  FaFacebookSquare,
+  FaGithubSquare,
+  FaInstagram,
+  FaTwitterSquare,
+} from "react-icons/fa";
+
+const Footer = () => {
+  return (
+    <div className="max-w-[1240px] mx-auto py-16 px-4 grid lg:grid-cols-3 gap-8 text-gray-300">
+      <div>
+        <h1 className="w-full text-3xl font-bold text-[#00df9a]">REACT.</h1>
+        <p className="py-4">
+          Unlock the power of your data today - sign up for our advanced
+          analytics platform and gain insights that drive your business forward!
+        </p>
+        <div className="flex justify-between md:w-[75%] my-6 cursor-pointer">
+          <FaFacebookSquare size={30} />
+          <FaInstagram size={30} />
+          <FaTwitterSquare size={30} />
+          <FaGithubSquare size={30} />
+          <FaDribbbleSquare size={30} />
+        </div>
+      </div>
+      <div className="lg:col-span-2 flex justify-between mt-6">
+        <div>
+          <h6 className="font-medium text-gray-400">Solutions</h6>
+          <ul className="cursor-pointer">
+            <li className="p-2 text-sm">Analytics</li>
+            <li className="p-2 text-sm">Marketing</li>
+            <li className="p-2 text-sm">Commerce</li>
+            <li className="p-2 text-sm">Insights</li>
+          </ul>
+        </div>
+        <div>
+          <h6 className="font-medium text-gray-400">Support</h6>
+          <ul className="cursor-pointer">
+            <li className="p-2 text-sm">Pricing</li>
+            <li className="p-2 text-sm">Documentation</li>
+            <li className="p-2 text-sm">Guides</li>
+            <li className="p-2 text-sm">API Status</li>
+          </ul>
+        </div>
+        <div>
+          <h6 className="font-medium text-gray-400">Company</h6>
+          <ul className="cursor-pointer">
+            <li className="p-2 text-sm">About</li>
+            <li className="p-2 text-sm">Blog</li>
+            <li className="p-2 text-sm">Jobs</li>
+            <li className="p-2 text-sm">Press</li>
+            <li className="p-2 text-sm">Careers</li>
+          </ul>
+        </div>
+        <div>
+          <h6 className="font-medium text-gray-400">Legal</h6>
+          <ul className="cursor-pointer">
+            <li className="p-2 text-sm">Claim</li>
+            <li className="p-2 text-sm">Policy</li>
+            <li className="p-2 text-sm">Terms</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
